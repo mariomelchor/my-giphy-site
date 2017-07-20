e13be468cfb92cb6a7d90fb846c8e45c5efcc5fc
mariomelchor <mario@mariomelchor.com>
2017-07-19 18:03:27 -0700

    Added array of tags submit button adds to array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+  // Search Tags Array
+  var giphyTags = [ 'badger', 'bat', 'bear', 'bird', 'bulldog', 'butterfly', 'camel', 'cat', 'caterpillar' ];
+
   var apiurl = 'api.giphy.com';
   var search = '/v1/gifs/search?';
   var apikey = 'dc6zaTOxFJmzC';
@@ -7,17 +10,43 @@ $(document).ready(function() {
   var animal = 'q=dogs'
   var queryURL = 'http://'+ apiurl + search + animal +'&limit=18&api_key=' + apikey;
 
+  // Displays tags in the DOM
+  function renderTags() {
+    $('#giphy-tags').empty();
+    $.each( giphyTags, function( index, val ) {
+       var giphyTag = $('<a href="#" class="btn btn-default btn-block">').html( val );
+       $('#giphy-tags').append( giphyTag );
+    });
+  }
+
+  renderTags();
+
+  // Get value of form input when clicking submit add it to array
+  $('#add-tag-submit').click(function(e) {
+    e.preventDefault();
+
+    var value = $('#add-tag-input').val();
+    giphyTags.push(value);
+
+    renderTags();
+
+  });
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).done(function (result) {
 
-    console.log(result);
+    // console.log(result);
     var giphy = result;
+    var gif;
 
    for ( var i = 0; i < giphy.data.length; i++ ) {
     var column = $('<div class="giphy-col col-sm-2">');
     var img    = $('<img>');
+
+    gif = giphy.data[i].images.downsized.url;
+    // console.log(gif);
 
     img.attr({
       src:    giphy.data[i].images.fixed_height_still.url,
@@ -32,8 +61,8 @@ $(document).ready(function() {
     $('#giphy-row').append( column );
 
     $(document).on('click', '.giphy-img', function(e) {
-      console.log('helo');
-     $(this).attr('src', giphy.data[i].images.fixed_height.url );
+      // console.log('helo');
+     $(this).attr('src', gif );
     });
 
    }
